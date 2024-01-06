a42f255dc1f878bdeeca19be59b0575865a6e334
MaciejDubowik <maciejxpoland@gmail.com>
2024-01-06 21:17:38 +0100

    ex. 45

---
+++ 45. Actors and Directors Who Cooperated at Least Three Times.sql
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/description/
+
+# Table ActorDirector
+
+# +-------------+---------+
+#  Column Name  Type    
+# +-------------+---------+
+#  actor_id     int     
+#  director_id  int     
+#  timestamp    int     
+# +-------------+---------+
+# timestamp is the primary key (column with unique values) for this table.
+ 
+
+# Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+# Return the result table in any order.
+
+# Write your MySQL query statement below
+SELECT
+    actor_id,
+    director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(actor_id) = 3;
